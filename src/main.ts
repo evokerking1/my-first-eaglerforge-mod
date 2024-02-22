03273b23a2b197d640c703075f0bb4530dddbdda
evokerking1 <118303818+evokerking1@users.noreply.github.com>
2024-02-22 16:10:53 +0000

    d

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { chatspammer } from "./chatspammer"
 
-console.log("starting yourmodname...");
+console.log("starting Click Notifier...");
 chatspammer();//(the chat spam is only visible to you)
