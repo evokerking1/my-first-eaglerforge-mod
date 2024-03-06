0d79752052888565297f0a53ad3bc21a2de84f80
evokerking1 <118303818+evokerking1@users.noreply.github.com>
2024-03-06 13:58:31 +0000

    Try to make spider hack from eaglerrerborn

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { chatspammer } from "./chatspammer"
+import { spiderhack } from "./test"
 
 console.log("Starting Click Notifier...");
 chatspammer();//(the chat spam is only visible to you)
+
+
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+export function spiderhack(){
+    function runCode(){
+        if()
+    }
+}
